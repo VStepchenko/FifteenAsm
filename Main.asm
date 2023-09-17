3464db5cd976d18c146f87cea78a270bfaea1711
VasiliyStepchenko <vasily.s@dearsystems.com>
2023-09-17 23:06:03 +0700

    simple window

--- Main.asm
+++ Main.asm
@@ -2,37 +2,163 @@
 .model flat, stdcall
 .stack 4096
 
-ExitProcess PROTO, dwExitCode:DWORD
-MessageBoxA PROTO,
-    hWnd:DWORD,
-    pText:PTR BYTE,
-    pCaption:PTR BYTE,
-    style:DWORD
-
-;   Or use the alternative declarations belows: 
-;   (If used these, also need to edited the "call" instructions' proc names)
-; extern ExitProcess@4 : PROC
-; extern MessageBoxA@16 : PROC
+include WinApiConstants.inc
+include WinApiProto.inc
 
 .data
-myText			db "Hello World", 0
-myCaption		db "Message Box Testing", 0
+include AppConstants.inc
+
+hInstance     dd 0
+commandLine   dd 0
+hWnd          dd 0
 
 .code
-main PROC
+start:
 
-        ; see: Win32 API MessageBox
-        ; https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
-    push 0                  ; MB_OK 
-    push offset myCaption   ; caption's start address
-    push offset myText      ; text's start address
-    push 0                  ; hwnd: Owner window's handle
-    call MessageBoxA        ; Or call MessageBoxA@16
-	
-        ; see: Win32 API ExitProcess
-        ; https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-exitprocess
-	push 12345678           ; exit code
-	call ExitProcess        ; Or call ExitProcess@4
-main ENDP
+    push NULL
+    call GetModuleHandleA
+    mov hInstance, eax
 
-END main        ;specifying the program's entry point
+    call GetCommandLineA
+    mov CommandLine, eax
+
+    push SW_SHOWDEFAULT
+    push CommandLine
+    push NULL
+    push hInstance
+    call WinMain
+
+    push eax
+    call ExitProcess
+
+WinMain proc hInst :DWORD, hPrevInst :DWORD, CmdLine :DWORD, CmdShow :DWORD
+
+    LOCAL wc   :WNDCLASSEX
+    LOCAL msg  :MSGSTRUCT
+
+    LOCAL Wwd  :DWORD
+    LOCAL Wht  :DWORD
+    LOCAL Wtx  :DWORD
+    LOCAL Wty  :DWORD
+
+    mov wc.cbSize,         sizeof WNDCLASSEX
+    mov wc.style,          CS_BYTEALIGNWINDOW
+    mov wc.lpfnWndProc,    offset WndProc
+    mov wc.cbClsExtra,     NULL
+    mov wc.cbWndExtra,     NULL
+    mov eax,               hInst
+    mov wc.hInstance,      eax
+    mov wc.hbrBackground,  COLOR_BTNSHADOW
+    mov wc.lpszMenuName,   NULL
+    mov wc.lpszClassName,  offset szClassName
+
+    push 500
+    push hInst
+    call LoadIconA
+    mov wc.hIcon, eax
+    
+    push IDC_ARROW
+    push NULL
+    call LoadCursorA
+    mov wc.hCursor, eax
+    mov wc.hIconSm, 0
+
+    lea eax, wc
+    push eax
+    call RegisterClassExA
+
+    ; Centre window at following size
+
+    mov Wwd, windowWidth
+    mov Wht, windowHeight
+
+    push SM_CXSCREEN
+    call GetSystemMetrics
+
+    push eax
+    push Wwd
+    call TopXY
+    mov Wtx, eax
+
+    push SM_CYSCREEN
+    call GetSystemMetrics
+
+    push eax
+    push Wht
+    call TopXY
+    mov Wty, eax
+
+    push NULL
+    push hInst
+    push NULL
+    push NULL
+    push Wht
+    push Wwd
+    push Wty
+    push Wtx
+    push WS_BORDER or WS_CAPTION or WS_SYSMENU or WS_MINIMIZEBOX
+    push offset caption
+    push offset szClassName
+    push WS_EX_OVERLAPPEDWINDOW or WS_EX_COMPOSITED ;magic word WS_EX_COMPOSITED to enable double buffering
+    call CreateWindowExA
+
+    mov hWnd,eax
+
+    push 600
+    push hInst
+    call LoadMenuA
+
+    push eax
+    push hWnd
+    call SetMenu
+
+    push SW_SHOWNORMAL
+    push hWnd
+    call ShowWindow
+
+    push hWnd
+    call UpdateWindow
+
+    ; Loop until PostQuitMessage is sent
+    .WHILE TRUE
+    invoke GetMessageA, ADDR msg, NULL, 0, 0
+    .BREAK .IF (!eax)
+        invoke TranslateMessage, ADDR msg
+        invoke DispatchMessageA, ADDR msg
+    .ENDW
+
+    mov eax, msg.wParam
+    ret
+
+WinMain endp
+
+WndProc proc hWin :DWORD, uMsg :DWORD, wParam :DWORD, lParam :DWORD
+
+    LOCAL Ps     :PAINTSTRUCT
+    LOCAL hDC    :DWORD
+
+    .IF uMsg == WM_DESTROY
+        invoke PostQuitMessage, NULL
+        xor eax, eax
+        ret
+    .ENDIF
+
+    invoke DefWindowProcA, hWin, uMsg, wParam, lParam
+
+    ret
+
+WndProc endp
+
+TopXY proc wDim:DWORD, sDim:DWORD
+
+    shr sDim, 1      ; divide screen dimension by 2
+    shr wDim, 1      ; divide window dimension by 2
+    mov eax, wDim    ; copy window dimension into eax
+    sub sDim, eax    ; sub half win dimension from half screen dimension
+
+    mov eax, sDim
+    ret
+
+TopXY endp
+
+END start
